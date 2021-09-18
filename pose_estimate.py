854bbe22996392defc7263f15fe4ebd1ad76b378
glenchiridza <glenchiridz@gmail.com>
2021-09-18 20:24:20 +0200

    play vid

--- pose_estimate.py
+++ pose_estimate.py
@@ -1,12 +1,23 @@
 import cv2
 import mediapipe as mp
+import time
 
-cap = cv2.VideoCapture("pose_videos/p1.avi")
+
+mpPose = mp.solutions.pose
+
+cap = cv2.VideoCapture("pose_videos/1.mp4")
+prev_time = 0
 
 while True:
     success, img = cap.read()
+
+    curr_time = time.time()
+    fps = 1/(curr_time - prev_time)
+    prev_time = curr_time
+
+    cv2.putText(img, str(int(fps)),(60,50), cv2.FONT_HERSHEY_PLAIN,3,(255,243,255,255),3)
+
     cv2.imshow("Image",img)
+
     cv2.waitKey(1)
 
-if __name__=="__main__":
-    pass
