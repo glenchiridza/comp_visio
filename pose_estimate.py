20a4fd44ca995e2b67b3d1dc660d48feb5fa7511
glenchiridza <glenchiridz@gmail.com>
2021-09-18 20:20:17 +0200

    post estimation

--- pose_estimate.py
+++ pose_estimate.py
@@ -1,10 +1,12 @@
 import cv2
 import mediapipe as mp
 
-cap = cv2.VideoCapture("poseVideos/1.mp4")
+cap = cv2.VideoCapture("pose_videos/p1.avi")
 
 while True:
     success, img = cap.read()
     cv2.imshow("Image",img)
     cv2.waitKey(1)
 
+if __name__=="__main__":
+    pass
