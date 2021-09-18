1838ec52abaf8b377778cc2cfa8d646b0d59d74f
glenchiridza <glenchiridz@gmail.com>
2021-09-18 13:47:59 +0200

    detector class:

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -2,7 +2,14 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+class handleDetector():
+    def __init__(self, mode=False,max_hands =2,
+                 detection_conf=0,5,track_conf=0.5):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_conf = detection_conf
+        self.track_conf = track_conf
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
@@ -10,8 +17,7 @@ mpDraw = mp.solutions.drawing_utils
 prevTime = 0
 currTime = 0
 
-while True:
-    success, img = cap.read()
+
     imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # hands only use RGB images
     results = hands.process(imageRGB)
@@ -24,29 +30,36 @@ while True:
             # get the landmark information, x,y coords
             for idx, lm in enumerate(handlms.landmark):
                 # get the pixels out of the image decimal values
-                height,width,channels = img.shape
-                cx, cy = int(lm.x*width), int(lm.y*height)
-                print(idx,cx,cy)
+                height, width, channels = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                print(idx, cx, cy)
                 # if id of landmark is 0->hand bottom end, 4 if tip of thumb, and others fingers 1,2,3
                 if idx == 0:
-                    cv2.circle(img, (cx,cy),15,(255,234,255),cv2.FILLED)
-
-
+                    cv2.circle(img, (cx, cy), 15, (255, 234, 255), cv2.FILLED)
 
             # use mediapipe function to draw all points on hand and there are almost 21 points
             mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
 
-    currTime = time.time()
-    fps = 1 / (currTime - prevTime)
-    prevTime = currTime
 
-    cv2.putText(img, str(int(fps)), (10, 90), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255, 255), 3)
+def main():
+    prevTime = 0
+    currTime = 0
+    cap = cv2.VideoCapture(0)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    while True:
+        success, img = cap.read()
+
+        currTime = time.time()
+        fps = 1 / (currTime - prevTime)
+        prevTime = currTime
+
+        cv2.putText(img, str(int(fps)), (10, 90), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
