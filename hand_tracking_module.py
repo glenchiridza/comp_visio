3540466a63d02306beb89e4e6bcc77bfab1bb87b
glenchiridza <glenchiridz@gmail.com>
2021-09-18 13:55:03 +0200

    module part1

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -19,7 +19,7 @@ class handleDetector():
                                         self.detection_conf,self.track_conf)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def locateHands(self, img):
+    def locateHands(self, img, draw=True):
         imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # hands only use RGB images
         results = self.hands.process(imageRGB)
@@ -29,28 +29,33 @@ class handleDetector():
         # extract multiple hands
         if results.multi_hand_landmarks:
             for handlms in results.multi_hand_landmarks:
-                # get the landmark information, x,y coords
-                for idx, lm in enumerate(handlms.landmark):
-                    # get the pixels out of the image decimal values
-                    height, width, channels = img.shape
-                    cx, cy = int(lm.x * width), int(lm.y * height)
-                    print(idx, cx, cy)
-                    # if id of landmark is 0->hand bottom end, 4 if tip of thumb, and others fingers 1,2,3
-                    if idx == 0:
-                        cv2.circle(img, (cx, cy), 15, (255, 234, 255), cv2.FILLED)
+                if draw:
+                    # use mediapipe function to draw all points on hand and there are almost 21 points
+                    self.mpDraw.draw_landmarks(img, handlms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-                # use mediapipe function to draw all points on hand and there are almost 21 points
-                self.mpDraw.draw_landmarks(img, handlms, self.mpHands.HAND_CONNECTIONS)
+
+
+# # get the landmark information, x,y coords
+#                 for idx, lm in enumerate(handlms.landmark):
+#                     # get the pixels out of the image decimal values
+#                     height, width, channels = img.shape
+#                     cx, cy = int(lm.x * width), int(lm.y * height)
+#                     print(idx, cx, cy)
+#                     # if id of landmark is 0->hand bottom end, 4 if tip of thumb, and others fingers 1,2,3
+#                     if idx == 0:
+#                         cv2.circle(img, (cx, cy), 15, (255, 234, 255), cv2.FILLED)
 
 
 def main():
     prevTime = 0
     currTime = 0
     cap = cv2.VideoCapture(0)
+    detector = handleDetector()
 
     while True:
         success, img = cap.read()
-
+        img = detector.locateHands(img)
         currTime = time.time()
         fps = 1 / (currTime - prevTime)
         prevTime = currTime
