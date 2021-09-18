7115cfc146bda53a19e4d106fab7355c6e2ec8b2
glenchiridza <glenchiridz@gmail.com>
2021-09-18 12:52:31 +0200

    results multi_hand_landmarks

--- hand_detection.py
+++ hand_detection.py
@@ -11,6 +11,15 @@ hands = mpHands.Hands()
 
 while True:
     success, img = cap.read()
+    imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # hands only use RGB images
+    results = hands.process(imageRGB)
+    print(results.multi_hand_landmarks)
+    # multi_hand_landmarks shows the coords if hand detected else None
+
+    # extract multiple hands
+
+
     cv2.imshow("Image",img)
     cv2.waitKey(1)
 
