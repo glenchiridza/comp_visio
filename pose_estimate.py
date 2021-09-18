175860ae3887e779951c584b7c6ad43c3a348c87
glenchiridza <glenchiridz@gmail.com>
2021-09-18 20:38:41 +0200

    get pixels from deci

--- pose_estimate.py
+++ pose_estimate.py
@@ -11,21 +11,23 @@ prev_time = 0
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    print(results.pose_landmarks)
 
     if results.pose_landmarks:
-        mpDraw.draw_landmarks(img,results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for idx, lm in enumerate(results.pose_landmarks.landmark):
+            height, width, channel = img.shape
+            print(lm.x, lm.y)
+            cx,cy = lm.x*width, lm.y*height
 
 
     curr_time = time.time()
-    fps = 1/(curr_time - prev_time)
+    fps = 1 / (curr_time - prev_time)
     prev_time = curr_time
 
-    cv2.putText(img, str(int(fps)),(60,50), cv2.FONT_HERSHEY_PLAIN,3,(255,243,255,255),3)
+    cv2.putText(img, str(int(fps)), (60, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 243, 255, 255), 3)
 
-    cv2.imshow("Image",img)
+    cv2.imshow("Image", img)
 
     cv2.waitKey(1)
-
