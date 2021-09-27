348e002a6cfa3f4a8cd7bf29daf3339d9068f0d5
glenchiridza <glenchiridz@gmail.com>
2021-09-27 22:45:06 +0200

    add face detection

--- face_detection.py
+++ face_detection.py
@@ -4,14 +4,17 @@ import time
 
 cap = cv2.VideoCapture("pose_videos/1.mp4")
 pTime = 0
+
+mpFaceDetection = mp.solutions.face_detection
+
+
 while True:
     success, img = cap.read()
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f"FPS: {int(fps)}",(10,50),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2)
-
+    cv2.putText(img, f"FPS: {int(fps)}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.waitKey(20)
