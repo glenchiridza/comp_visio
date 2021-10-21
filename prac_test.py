cb9dfb7edec13adb51c1ec2e5e67933747f3898c
glenchiridza <glenchiridz@gmail.com>
2021-10-21 20:04:05 +0200

    hand connections

--- prac_test.py
+++ prac_test.py
@@ -6,6 +6,7 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 
 
@@ -16,6 +17,9 @@ while True:
 
     # print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
-        for handlms in results
+        for handlms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+
+
     cv2.imshow("Image",img)
     cv2.waitKey(1)
