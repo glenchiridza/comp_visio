dab1ede47a7e18c2dc1c09a64751d2a33693d480
glenchiridza <glenchiridz@gmail.com>
2021-10-21 21:46:36 +0200

    fix done

--- prac_test_module.py
+++ prac_test_module.py
@@ -18,41 +18,56 @@ class hand_detector():
         )
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self,img, draw=True):
+    def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handlms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handlms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handlms, self.mpHands.HAND_CONNECTIONS)
 
-                # for idx, lm in enumerate(handlms.landmark):
-                #     h, w, channel = img.shape
-                #     cx, cy = int(lm.x * w), int(lm.y * h)
-                #     print(idx, cx, cy)
-                #     if idx == 0:
-                #         cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        return img
+
+    def find_position(self, img, handNo=0, draw=True):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            myHands = self.results.multi_hand_landmarks[handNo]
+            # get the landmark information, x,y coords
+            for idx, lm in enumerate(myHands.landmark):
+                # get the pixels out of the image decimal values
+                height, width, channels = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                # print(idx, cx, cy)
+                lm_list.append([idx, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 8, (255, 234, 255), cv2.FILLED)
+
+        return lm_list
 
 
 def main():
+
     prevTime = 0
     currTime = 0
     cap = cv2.VideoCapture(0)
+    detector = hand_detector()
 
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.find_position(img)
+        if len(lmList) != 0:
+            print(lmList[4])
 
         currTime = time.time()
         fps = 1 / (currTime - prevTime)
         prevTime = currTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 255, 255, 255), 3)
+        cv2.putText(img, str(int(fps)), (10, 90), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255, 255), 3)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
-
 
 if __name__ == "__main__":
     main()
