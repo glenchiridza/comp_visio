9993221a84767b34fdf114e8c7cad6af515cd6b3
glenchiridza <glenchiridz@gmail.com>
2021-10-26 23:01:21 +0200

    pose connections

--- tester.py
+++ tester.py
@@ -1,10 +1,30 @@
 import cv2
 import mediapipe as mp
+import time
 
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture("pose_videos/1.mp4")
-
+prev_time = 0
 while True:
-    success,img = cap.read()
-    cv2.imshow("Image",img)
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
+
+
+
+    curr_time = time.time()
+    fps = 1 / (curr_time - prev_time)
+    prev_time = curr_time
+    cv2.putText(img, str(int(fps)), (72, 55), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
