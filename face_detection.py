33b4f31fed6f0a48c96ead3e507d1c3fb5f2658a
glenchiridza <glenchiridz@gmail.com>
2021-10-27 22:20:51 +0200

    face module

--- face_detection.py
+++ face_detection.py
@@ -26,6 +26,7 @@ while True:
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                     int(bboxC.width * iw), int(bboxC.height * ih)
             cv2.rectangle(img,bbox, (255,0,255),2)
+            cv2.putText(img, f"{int(detection.score[0] * 100)}%", (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0,255), 2)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
---
+++ face_detection_module.py
@@ -0,0 +1,53 @@
+import cv2
+import mediapipe as mp
+import time
+
+class FaceDetector:
+    def __init__(self, min_detection_conf=0.5):
+        self.min_detection_conf =
+
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+face_detection = mpFaceDetection.FaceDetection(0.75)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(imgRGB)
+
+    if results.detections:
+        for idx, detection in enumerate(results.detections):
+            # mpDraw.draw_detection(img, detection)
+            # print(idx, detection)
+            # print(detection.score)
+            # print(detection.location_data.relative_bounding_box)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f"{int(detection.score[0] * 100)}%", (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 255), 2)
+
+
+
+
+def main():
+    cap = cv2.VideoCapture("pose_videos/1.mp4")
+    pTime = 0
+
+    while True:
+        success, img = cap.read()
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f"FPS: {int(fps)}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(20)
+
+
+if __name__ == "__main__":
+    main()
