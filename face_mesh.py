01611a0a221e486201a0764422e2cf1e1fbf62c8
glenchiridza <glenchiridz@gmail.com>
2021-10-28 22:07:03 +0200

    draw_specs

--- face_mesh.py
+++ face_mesh.py
@@ -9,11 +9,21 @@ mpDraw = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=2)
 
+# change the size,  thickness of the circles or lines around it
+draw_spec = mpDraw.DrawingSpec(thickness=1, circle_radius=10)
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(imgRGB)
+
+    # display the results
+    if results.multi_face_landmarks:
+        for face_lm in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img,
+                                  face_lm,
+                                  mp_face_mesh.FACE_CONNECTIONS,
+                                  draw_spec, draw_spec)
 
     cur_time = time.time()
     fps = 1 / (cur_time - prev_time)
