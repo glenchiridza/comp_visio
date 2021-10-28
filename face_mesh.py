df0e3f8c69ebe27b5a20208848e1812e040da4a0
glenchiridza <glenchiridz@gmail.com>
2021-10-28 22:00:59 +0200

    imgRGB input

--- face_mesh.py
+++ face_mesh.py
@@ -4,8 +4,16 @@ import time
 
 cap = cv2.VideoCapture("pose_videos/1.mp4")
 prev_time = 0
+
+mpDraw = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=2)
+
+
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(imgRGB)
 
     cur_time = time.time()
     fps = 1 / (cur_time - prev_time)
