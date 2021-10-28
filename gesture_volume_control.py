a7d836d6681d7b953abea4991725b23d33e0893f
glenchiridza <glenchiridz@gmail.com>
2021-10-28 23:44:04 +0200

    detect fix

--- gesture_volume_control.py
+++ gesture_volume_control.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import math
 import numpy as np
 
 # my modules import
@@ -12,27 +13,33 @@ cap.set(3, cam_width)
 cap.set(4, cam_height)
 prev_time = 0
 
-detector = htm.handleDetector(detection_conf=0.75)
+detector = htm.handleDetector(detection_conf=0.5)
 
 while True:
     success, img = cap.read()
     img = detector.locateHands(img)
-    lm_list = detector.locatePosition(img, draw=False)
+    lm_list = detector.locatePosition(img)
     if len(lm_list) != 0:
         # get tips of both thumb and index => 4and8 respectively
         print(lm_list[4], lm_list[8])
 
-        x1,y1 = lm_list[4][1], lm_list[4][2]
-        x2,y2 = lm_list[8][1], lm_list[8][2]
+        x1, y1 = lm_list[4][1], lm_list[4][2]
+        x2, y2 = lm_list[8][1], lm_list[8][2]
 
-        #get center of line
-        cx,cy = (x1+x2)//2, (y1+y2)//2
+        # get center of line
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        cv2.circle(img, (x1,y1),10,(255,0,255),cv2.FILLED)
-        cv2.circle(img, (x2,y2),10,(255,0,255),cv2.FILLED)
-        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
-        cv2.circle(img,(cx,cy),10,(255,0,255),cv2.FILLED)
+        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
 
+        # get length of line
+        length = math.hypot(x2-x1,y2-y1)
+        print(length)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 10, (0, 255,0), cv2.FILLED)
 
     curr_time = time.time()
     fps = 1 / (curr_time - prev_time)
