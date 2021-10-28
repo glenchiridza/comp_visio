bda93985739bb0b09806326ba484396fd0c46d86
glenchiridza <glenchiridz@gmail.com>
2021-10-28 22:52:02 +0200

    drawing in func

--- face_mesh_module.py
+++ face_mesh_module.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import time
 
 
-class FaceMesh:
+class FaceMeshDetector:
     def __init__(self, static_mode=False, max_faces=2,
                  min_detection_conf=0.5,
                  min_track_conf=0.5):
@@ -20,7 +20,7 @@ class FaceMesh:
         # change the size,  thickness of the circles or lines around it
         self.draw_spec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
-    def find_face_mesh(self,img,draw=True):
+    def find_face_mesh(self, img, draw=True):
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.face_mesh.process(imgRGB)
@@ -28,21 +28,24 @@ class FaceMesh:
         # display the results
         if results.multi_face_landmarks:
             for face_lm in results.multi_face_landmarks:
-                self.mpDraw.draw_landmarks(img,
-                                      face_lm,
-                                      self.mp_face_mesh.FACE_CONNECTIONS,
-                                      self.draw_spec, self.draw_spec)
+                if draw:
+                    self.mpDraw.draw_landmarks(img,
+                                               face_lm,
+                                               self.mp_face_mesh.FACE_CONNECTIONS,
+                                               self.draw_spec, self.draw_spec)
                 # find all the different points
                 for lm in face_lm.landmark:
                     ih, iw, ic = img.shape
                     x, y = int(lm.x * iw), int(lm.y * ih)
-
+        return img
 
 def main():
     cap = cv2.VideoCapture(0)
     prev_time = 0
+    detector = FaceMeshDetector
     while True:
         success, img = cap.read()
+        img = detector.find_face_mesh(img)
         cur_time = time.time()
         fps = 1 / (cur_time - prev_time)
         prev_time = cur_time
