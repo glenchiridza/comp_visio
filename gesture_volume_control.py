eaad0953756978f61dfc500fbb1aa9b4901301ca
glenchiridza <glenchiridz@gmail.com>
2021-10-28 23:17:24 +0200

    reusing hand module

--- gesture_volume_control.py
+++ gesture_volume_control.py
@@ -2,10 +2,30 @@ import cv2
 import time
 import numpy as np
 
+#my modules import
+import hand_tracking_module as htm
+
+
+cam_width, cam_height = 640, 480
+
 cap = cv2.VideoCapture(0)
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+prev_time = 0
+
+detector = htm.handleDetector()
+
 
 while True:
-    success,img = cap.read()
+    success, img = cap.read()
+    detector.locateHands(img)
 
-    cv2.imshow("ImageGC",img)
+
+    curr_time = time.time()
+    fps = 1 / (curr_time - prev_time)
+    prev_time = curr_time
+
+    cv2.putText(img, f"fps{int(fps)}", (40, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 3)
+
+    cv2.imshow("ImageGC", img)
     cv2.waitKey(1)
