dbde59f477b88f0ce5bfdb8b076cd19f9556718a
glenchiridza <glenchiridz@gmail.com>
2021-10-29 22:29:34 +0200

    image canny

--- open_cv/img_color_spaces.py
+++ open_cv/img_color_spaces.py
@@ -4,6 +4,11 @@ img = cv2.imread("res/me.jpg")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("GrayScale Image", imgGray)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
 
+# edge detector, canny edge detector
+imgCanny = cv2.Canny(img, 100, 100)
+
+cv2.imshow("GrayScale Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
 cv2.waitKey(0)
