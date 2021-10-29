c28f1335eb3821d5a53e9df5d739bd40bdd1c5ad
glenchiridza <glenchiridz@gmail.com>
2021-10-29 22:23:50 +0200

    vid capture done

--- open_cv/video_capture.py
+++ open_cv/video_capture.py
@@ -1,4 +1,14 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
+# vid size 3 = width, 4 = height
+vid.set(3, 640)
+vid.set(4, 480)
+# vid brightness id = 10
+vid.set(10, 100)
 
+while True:
+    success, img = vid.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
