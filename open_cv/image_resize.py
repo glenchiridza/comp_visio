0fdc510029348e621201422bd37161e7c7e0135c
glenchiridza <glenchiridz@gmail.com>
2021-11-26 09:19:25 +0200

    image reshape done

--- open_cv/image_resize.py
+++ open_cv/image_resize.py
@@ -1,7 +1,17 @@
 import cv2
 
-img = cv2.imread("res/runner.png")
+img = cv2.imread("res/msutop.jpg")
+print(img.shape)
 
-cv2.imshow(img)
+# cv2.imshow("School Top",img)
+
+imgResize = cv2.resize(img, (500,700))  # on resize we have the width first argument and height as second argument
+cv2.imshow("School Top",imgResize)
+print(imgResize.shape)
+
+#image cropping
+imgCropped = img[0:200,240:450] # here we have the height as first argument and width as the second
+
+cv2.imshow("Cropped Image",imgCropped)
 
 cv2.waitKey(0)
