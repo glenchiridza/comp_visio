fa4a834a619c8a33db6cbe64769999e579328ecd
glenchiridza <glenchiridz@gmail.com>
2021-11-26 10:27:47 +0200

    hsv resuze

---
+++ open_cv/color_detection.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+def empty(a):
+    pass
+
+path = "res/msutop.jpg"
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty) # hue is actually 360 but in open cv its values end at 179, like 0-180 so we put 179
+cv2.createTrackbar("Hue Max","TrackBars",179,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",255,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+
+
+# keep looping through and get current values
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars") # the spelling has to be the same as define in createTrackBar, and namedWindow
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_max,h_min,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+
+    cv2.imshow("Origin",img)
+    cv2.imshow("HSV image",imgHSV)
+    cv2.imshow("HSV image Masked",mask)
+    cv2.waitKey(0)
--- open_cv/join_images.py
+++ open_cv/join_images.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+# note both of images should have the same channels, if gray scale both should be gray scale, if colored both should be colored
+# cant resize
+
 img = cv2.imread("res/bc.jpg")
 
 imgHor = np.hstack((img,img))
