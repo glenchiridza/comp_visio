21e36a2a568bd55b4938071d91b063f8d320bd25
glenchiridza <glenchiridz@gmail.com>
2021-11-26 09:10:44 +0200

    image color spaces done

--- open_cv/img_color_spaces.py
+++ open_cv/img_color_spaces.py
@@ -2,15 +2,19 @@ import cv2
 import numpy as np
 
 img = cv2.imread("res/runner.png")
+kernel = np.ones((5,5), np.uint8)
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
 imgCanny = cv2.Canny(img,150,200)
-imgDialation = cv2.dilate(imgCanny,)
+imgDilation = cv2.dilate(imgCanny,kernel, iterations=1)
+imgEroded = cv2.erode(imgDilation,kernel,iterations=1)
 
 cv2.imshow("Image Output",imgGray)
 cv2.imshow("Image Blur",imgBlur)
 cv2.imshow("Image Canny",imgCanny)
+cv2.imshow("Image Dilation",imgDilation)
+cv2.imshow("Image Eroded",imgEroded)
 
 cv2.waitKey(0)
 
